271d3bb6b652eb7c65dc17f790d7a8c817644106
chen jian <32725087+Seaquakear@users.noreply.github.com>
2023-02-07 22:09:38 +0800

    DW2-V1.0
    DW2 V1.0版本，开源版本。

---
+++ Frm_Main.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace DW
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button_getdata = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader10,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 61);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(775, 503);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "大赚！";
+            this.columnHeader10.Width = 50;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "代码";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "名称";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "价格";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "涨停价";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "涨幅";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "是否新高";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "成交额";
+            this.columnHeader7.Width = 160;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "量比";
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "换手率";
+            // 
+            // button_getdata
+            // 
+            this.button_getdata.Location = new System.Drawing.Point(12, 12);
+            this.button_getdata.Name = "button_getdata";
+            this.button_getdata.Size = new System.Drawing.Size(115, 43);
+            this.button_getdata.TabIndex = 3;
+            this.button_getdata.Text = "获取实时数据";
+            this.button_getdata.UseVisualStyleBackColor = true;
+            this.button_getdata.Click += new System.EventHandler(this.button_getdata_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(793, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(643, 333);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("仿宋", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(794, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(655, 147);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "使用说明：\r\n1、请在交易日9点50分后再使用该程序，否则可能会取不到数\r\n据。交易时段每10分钟会更新一次数据，请手动点击\"获取实时\r\n数据\"按钮。\r\n2、鼠标" +
+    "左键双击列表中的股票，右侧会显示实时日K。\r\n3、列表数据按照涨幅从高到低排列。\r\n4、更多功能，请持续关注微信公众号“小建数码”，感谢支持!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(798, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1448, 576);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button_getdata);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Frm_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "强势股池（未开通创业板和科创板数据）";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.Button button_getdata;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Frm_Main.cs
@@ -0,0 +1,193 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace DW
+{
+    public partial class Frm_Main : Form
+    {
+        public static string licence = "";
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            
+            this.label2.Text = "当前licence:" + GetLicenceFromAppConfig();
+        }
+
+        private void button_getdata_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        private string GetLicenceFromAppConfig()
+        {
+            licence = ConfigurationManager.AppSettings["licence"];
+            return licence;
+        }
+
+        private static string HttpGet(string Url)
+        {
+            try
+            {
+                string retString = string.Empty;
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+                request.Method = "GET";
+                request.ContentType = "application/json;charset=utf-8";
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream myResponseStream = response.GetResponseStream();
+                StreamReader streamReader = new StreamReader(myResponseStream, Encoding.UTF8);
+                retString = streamReader.ReadToEnd();
+                streamReader.Close();
+                myResponseStream.Close();
+                return retString;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void GetData()
+        {
+            try
+            {
+                this.listView1.Items.Clear();
+                string nowTime = DateTime.Now.ToString("yyyy-MM-dd");
+                string url = "http://api.mycodenobug.com/api/oneparam/qsgc?datetime=" + nowTime  + "&licence=" + licence;
+
+                //get请求获取数据
+                string result = HttpGet(url);
+                if (result.Length > 3)
+                {
+                    var data = JsonConvert.DeserializeObject<List<QSGC>>(result);
+                    ListViewItem item;
+                    string kk = data[0].cje;
+                    for (int i = 0; i < data.Count; i++)
+                    {
+                        if (data[i].dm.ToString().Substring(0, 3) != "sz3" && data[i].dm.ToString().Substring(0, 5) != "sh688")
+                        {
+                            item = new ListViewItem();
+                            item.SubItems.Add(data[i].dm);
+                            item.SubItems.Add(data[i].mc);
+                            item.SubItems.Add(data[i].p);
+                            item.SubItems.Add(data[i].ztp);
+                            item.SubItems.Add(data[i].zf);
+                            item.SubItems.Add(data[i].nh);
+                            item.SubItems.Add(data[i].cje);
+                            item.SubItems.Add(data[i].lb);
+                            item.SubItems.Add(data[i].hs);
+                            listView1.Items.Add(item);
+                        }
+                    }
+
+                    //for (int i = 0; i < data.Count; i++)
+                    //{
+                    //    item = new ListViewItem();
+                    //    item.SubItems.Add(data[i].dm);
+                    //    item.SubItems.Add(data[i].mc);
+                    //    item.SubItems.Add(data[i].p);
+                    //    item.SubItems.Add(data[i].ztp);
+                    //    item.SubItems.Add(data[i].zf);
+                    //    item.SubItems.Add(data[i].nh);
+                    //    item.SubItems.Add(data[i].cje);
+                    //    item.SubItems.Add(data[i].lb);
+                    //    item.SubItems.Add(data[i].hs);
+                    //    listView1.Items.Add(item);
+                    //}
+                }
+                else
+                {
+                    MessageBox.Show("未到监控时间段，请在交易日9点50分后使用该程序！");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.listView1.SelectedItems.Count > 0)
+            {
+                string gp_code = listView1.SelectedItems[0].SubItems[1].Text;
+                FillPicture1(gp_code);
+            }
+        }
+
+        private void FillPicture1(string gp_code)
+        {
+            string url = "https://image.sinajs.cn/newchart/daily/n/" + gp_code + ".gif";
+            System.Net.WebClient _Client = new System.Net.WebClient();
+            byte[] _ImageByte = _Client.DownloadData(url);
+            System.IO.MemoryStream _ImageStrem = new System.IO.MemoryStream(_ImageByte);
+            Image _DownImage = Image.FromStream(_ImageStrem);
+            this.pictureBox1.Image = _DownImage;
+        }
+
+        public class QSGC
+        {
+            /// <summary>
+            /// 代码
+            /// </summary>
+            public string dm { get; set; }
+
+            /// <summary>
+            /// 名称
+            /// </summary>
+            public string mc { get; set; }
+
+            /// <summary>
+            /// 价格
+            /// </summary>
+            public string p { get; set; }
+
+            /// <summary>
+            /// 涨停价
+            /// </summary>
+            public string ztp { get; set; }
+            /// <summary>
+            /// 涨幅
+            /// </summary>
+            public string zf { get; set; }
+            /// <summary>
+            /// 成交额
+            /// </summary>
+            public string cje { get; set; }
+
+            /// <summary>
+            /// 是否新高
+            /// </summary>
+            public string nh { get; set; }
+
+            /// <summary>
+            /// 量比
+            /// </summary>
+            public string lb { get; set; }
+
+            /// <summary>
+            /// 换手率
+            /// </summary>
+            public string hs { get; set; }
+        }
+    }
+}
